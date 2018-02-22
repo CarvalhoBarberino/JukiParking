82bf80549c5ab9bfca71a28ca312410d565d6f38
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-02-22 00:18:32 -0300

    Melhoria na classe "Entrada"
    Mudança no nome de alguns botoes e recorte dos comandos "add" que estavam no metodo "actionPerformed" para o metodo construtor. Esta ultima mudança corrigiu o bug que fazia aparecer mais e mais botoes cada vez que entrava na janela CadastrandoCarro

--- JukiParking/src/Entrada.java
+++ JukiParking/src/Entrada.java
@@ -6,27 +6,33 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Entrada extends JFrame implements ActionListener{
-	JFrame j;//Este objeto ainda não foi inicializado mas foi declarado. Sera utilizado mais tarde
+	JFrame j;//Este objeto ainda nï¿½o foi inicializado mas foi declarado. Sera utilizado mais tarde
 	public Entrada(JFrame janelaAFechar) {//O metodo construtor recebe a referencia a janela principal.
-		System.out.println("fim do Entrada()");
+		setTitle("JukiParking\\CadastrandoCarro");
 		j = janelaAFechar;// j recebe a referencia da janela principal, que sera ocultada no actionPerformed
+		JButton botaoOk = new JButton("Ok");
+		JButton botaoCancelar = new JButton("Cancelar");
+		setSize(403, 250);
+		setLocation(150, 150);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
+		add(botaoOk);
+		add(botaoCancelar);
+		botaoCancelar.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setVisible(false);
+				j.setVisible(true);
+			}
+		});
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		new JFrame();
-		setTitle("JukiParking\\CadastrandoCarro");
+		
 		j.setVisible(false);//oculta a janela principal
-		JButton botaoCarroEntrando = new JButton("bla bla");
-		JButton botaoCarroSaindo = new JButton("bla bla bla");//testeGitHub
-		setSize(403, 250);
-		setLocation(150, 150);
-		setResizable(false);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
-		add(botaoCarroEntrando);
-		add(botaoCarroSaindo);
 		setVisible(true);
 	}
 
