630405a6b2d4d89d0f8eb33f91e3c87c1308b70f
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-03-14 18:44:25 -0300

    Add files via upload

--- JukiParking/src/Saida.java
+++ JukiParking/src/Saida.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 
 public class Saida extends JFrame implements ActionListener {
 	JFrame j;//Este objeto ainda n�o foi inicializado mas foi declarado. Sera utilizado mais tarde
+	long tempoMs;// Tempo em milisegundos
 	public Saida(JFrame janelaAFechar) {
 		setTitle("JukiParking\\CadastrandoSaida");
 		j = janelaAFechar;// j recebe a referencia da janela principal, que sera ocultada no actionPerformed
@@ -23,6 +24,8 @@ public class Saida extends JFrame implements ActionListener {
 		add(tPlaca);
 		JButton botaoOk = new JButton("Ok");
 		JButton botaoVoltar = new JButton("Voltar");
+		JLabel lHoras = new JLabel();
+		lHoras.setText("");
 		setSize(403, 250);
 		setLocation(150, 150);
 		setResizable(false);
@@ -37,22 +40,22 @@ public class Saida extends JFrame implements ActionListener {
 					ObjectInputStream oi = new ObjectInputStream(fi);
 					Gravavel x =(Gravavel) oi.readObject();
 					oi.close();
-					boolean entradaCorrespondente = false;
-					for(int i = x.vetorRegistros.size() - 1; (i > 0) && !entradaCorrespondente; i--) {
+					for(int i = x.vetorRegistros.size() - 1; i >= 0; i--) {
+						System.out.println("Lendo " + x.vetorRegistros.get(i).placa + " " + x.vetorRegistros.get(i).modelo + " " + x.vetorRegistros.get(i).cor);
 						if((x.vetorRegistros.get(i).placa.equals(tPlaca.getText())) && (x.vetorRegistros.get(i).dataSaida == null)) {
-							i = 0;
 							System.out.println("Achado " + x.vetorRegistros.get(i).placa + " " + x.vetorRegistros.get(i).modelo + " " + x.vetorRegistros.get(i).cor);
-							
+							x.vetorRegistros.get(i).dataSaida = new Date();
+							tempoMs = x.vetorRegistros.get(i).dataSaida.getTime() - x.vetorRegistros.get(i).dataEntrada.getTime();
+							lHoras.setText(String.valueOf((float)(tempoMs/3600000)) + " horas no estacionamento\nTotal a pagar: R$" + String.valueOf((float)(12 * tempoMs/3600000)));
 						}
 					}
-					/*FileOutputStream fo = new FileOutputStream("Registros.dat");
+					FileOutputStream fo = new FileOutputStream("Registros.dat");
 					ObjectOutputStream oo = new ObjectOutputStream(fo);
-					x.vetorRegistros.add(registroDeEntrada);
 					x.timeStamp = new Date();
 					x.seq++;
 					System.out.println("seq = " + x.seq);
 					oo.writeObject(x);
-					oo.close();*/
+					oo.close();
 				}
 				catch(Exception err){err.printStackTrace();}
 			}
@@ -65,6 +68,7 @@ public class Saida extends JFrame implements ActionListener {
 				dispose();
 			}
 		});
+		add(lHoras);
 	}
 	
 	@Override
